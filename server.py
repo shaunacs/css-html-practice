17dd9a9ac701a4e5251ea42a154ee7dc0a80112a
Shauna Saunders <shaunacsaunders@outlook.com>
2021-08-05 12:46:40 -0400

    Created html file for homepage

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+app.secret_key = "dev"
+
+@app.route('/')
+def render_homepage():
+    """Renders the homepage"""
+
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True, use_debugger=True)
